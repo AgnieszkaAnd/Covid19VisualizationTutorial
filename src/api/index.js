0fb6f2f813546e9069665c546edc3d10884e6134
AgnieszkaAnd <ag2rio@gmail.com>
2020-05-02 23:05:14 +0200

    added basic data display (3 card containers)

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,16 @@
 // functions to fetch data that we need
+
+// to make API requests
+import axios from 'axios';
+
+const url='https://covid19.mathdro.id/api';
+
+export const fetchData = async () => {
+    try {
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+        
+        return { confirmed, recovered, deaths, lastUpdate };
+    } catch (error) {
+
+    }
+}
